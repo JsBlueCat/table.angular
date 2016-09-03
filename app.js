bc9b6737fb23e5f379ff68ae385cf63485c20232
JsBlueCat <15618561709@163.com>
2016-09-03 09:28:07 +0800

    自定义 表格

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 var MyApp = angular.module('DynamicTable', ['Ui.Cery']);
 
 MyApp.controller('baseCtrl', ['$scope', function($scope) {
-
+    /*
+        Type 属性代表表格中的表现形式
+        0 文字
+        1 输入框
+        2 选择框
+     */
+    /*
+        Position 属性代表所处的位置
+        0 靠左
+        1 靠中
+        2 靠右
+     */
+    $scope.tableTitles = [{
+        name: '序号',
+        type: 0,
+        position: 0
+    }, {
+        name: '名称',
+        type: 0,
+        position: 1
+    }, {
+        name: '其他',
+        type: 0,
+        position: 2
+    }, {
+        name: '其他',
+        type: 0,
+        position: 2
+    }, ]
 
 
 }])
--- js/directives.js
+++ js/directives.js
@@ -1,8 +1,16 @@
 angular.module('Ui.Cery', [])
-.directive('dynamic-table',function(){
-    return {
-        restrict: 'E',
-        template: '<span>Hi there</span>',
-        replace: true
-    }
-})
+    .directive('dynamicTable', function() {
+        return {
+            scope: {
+                titles: '=',
+
+            },
+            restrict: 'E',
+            templateUrl: 'js/table.html',
+            transclude: true,
+            replace: true,
+            link: function(scope, element, attr) {
+
+            }
+        }
+    })
